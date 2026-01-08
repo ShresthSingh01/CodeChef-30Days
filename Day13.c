ed1ca5942e15db93b9af7c600e7740804d6fc25e
SHRESTH SINGH <shresthsingh112233@gmail.com>
2026-01-08 21:03:46 +0530

    Add files via upload

---
+++ Day13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+   
+    while (num > 0) {
+        printf("%d\n", num % 10);  
+        num /= 10;                
+    }
+
+    return 0;
+}
